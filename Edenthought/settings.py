e8d9b13e75404f467248b91ee562f1e573f0c70c
berichonq <quinnberichon@gmail.com>
2023-07-05 10:30:05 -0400

    Update settings.py

--- Edenthought/settings.py
+++ Edenthought/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['development-production.up.railway.app']
 
@@ -160,7 +160,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'images/'
 
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATICFILES_DIRS = [ BASE_DIR / '/app/static' ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
