b5084a7e3e354ad27bbd28b36b4b51e55989d33c
berichonq <quinnberichon@gmail.com>
2023-07-05 09:57:05 -0400

    Update settings.py

--- Edenthought/settings.py
+++ Edenthought/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['development-production.up.railway.app']
 
-#CSRF_TRUSTED_ORIGINS = ['https://']
+CSRF_TRUSTED_ORIGINS = ['https://']
 
 # Application definition
 
